0b879fb04143edc0cee79f518ccc02e3b56b8cfc
svaichu <s.vaichu@gmail.com>
2026-08-26 19:30:23 +0200

    update

--- getZeros.m
+++ getZeros.m
@@ -1,8 +1,8 @@
 function zeros = getZeros(A, B, C, D)
     syms s; % Define symbolic variable
     n = size(A,1);
-    M = [s*eye(n)-A, -B; C, D];
-    zero_eq = det(M); % Compute determinant symbolically
+    Rosenbrock_Matrix = [s*eye(n)-A, -B; C, D];
+    zero_eq = det(Rosenbrock_Matrix); % Compute determinant symbolically
     zeros = vpasolve(zero_eq, s); % Use vpasolve for numerical solutions
     zeros = double(zeros); % Convert symbolic results to double
 end
